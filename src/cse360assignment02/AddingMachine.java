74ad72a4ea39310f5b38be924cddf41e6028fcbe
MatthewWatson <mwatson@mantisag-tech.com>
2020-10-06 21:38:58 -0700

    Initial commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    /**
+     * Constructor
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * Returns the 'total' member variable
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * Adds parameter to 'total' member variable
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * Subtracts parameter from 'total' member variable
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * Prints transaction history
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * Resets AddingMachine instance
+     */
+    public void clear() {
+    }
+}
